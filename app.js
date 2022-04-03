495ea4b0c4f0b28a1966662816e4d931742ba604
fstaaa <fausta35tiawan@gmail.com>
2022-04-03 12:41:28 +0700

    genjreng

--- app.js
+++ app.js
@@ -42,11 +42,11 @@ type koneksi : single,pool and request
 -------------------------------------------*/
 app.use(
     connection(mysql,{
-        host: process.env.MYSQLHOST,
-        user: process.env.MYSQLUSER, // your mysql user
-        password : process.env.MYSQLPASSWORD, // your mysql password
-        port : process.env.MYSQLPORT, //port mysql
-        database:process.env.MYSQLDATABASE // your database name
+        host: "containers-us-west-35.railway.app",
+        user: "root", // your mysql user
+        password : "zJ1LoTDqZ1FJcxivG1Ju", // your mysql password
+        port : 6025, //port mysql
+        database : "railway", // your database name
     },'pool') //or single
 );
 app.use('/', index);
